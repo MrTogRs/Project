fbfb9dcbd1ac9a969f871bb1c8961351d4ed4ec5
MrTogRs <56183971+MrTogRs@users.noreply.github.com>
2019-11-03 06:50:21 +0700

    Add files via upload

--- Check.py
+++ Check.py
@@ -1,2 +1,13 @@
 class Check:
-    pass
+    def __init__(self, a, b, c):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+
+    def check_values(self):
+        try:
+            if ((type(float(self.__a)) and type(float(self.__b)) and
+               type(float(self.__b)) and type(float(self.__c))) is float) and self.__a != 0:
+                return True
+        except ValueError:
+            return False
--- Main.py
+++ Main.py
@@ -6,9 +6,12 @@ a = input()
 b = input()
 c = input()
 
-my_check = Check
+my_check = Check(a, b, c)
 
-my_dis = Discriminant(a, b, c)
-print(my_dis.get_discriminant())
-my_roots = Roots(a, b, my_dis.get_discriminant())
-print(my_roots.get_roots())
+if my_check.check_values() is True:
+    my_dis = Discriminant(a, b, c)
+    print(my_dis.get_discriminant())
+    my_roots = Roots(a, b, my_dis.get_discriminant())
+    print(my_roots.get_roots())
+else:
+    print("Введите числа")
