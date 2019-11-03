c5d21970d3486cf93fc56b4cf1c6c520a7dcd8a1
FlashgoKless <29282634+FlashgoKless@users.noreply.github.com>
2019-11-03 12:28:51 +0700

    Update check.py

--- Check.py
+++ Check.py
@@ -7,8 +7,10 @@ class Check:
     def check_type(self):
         try:
             if ((type(float(self.__a)) is float:
+                return True
             if ((type(float(self.__b)) is float:
+                return True
             if ((type(float(self.__c)) is float:
-            return True
+                return True
         except ValueError:
             return False
